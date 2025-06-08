0baf7deadf00952d7629ed938eb74eae4b8a6478
8888 <leecostello1@gmail.com>
2025-06-08 16:22:20 -0400

    Add CloudFront Origin Access Control and bucket policy for static assets; update package.json with staging deployment script

--- app/finn-infra/lib/finn-infra-stack.ts
+++ app/finn-infra/lib/finn-infra-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class FinnInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,15 +16,40 @@ export class FinnInfraStack extends cdk.Stack {
       autoDeleteObjects: true, // For development - remove for production
     });
 
+    // Create Origin Access Control for CloudFront
+    const originAccessControl = new cloudfront.CfnOriginAccessControl(this, 'OriginAccessControl', {
+      originAccessControlConfig: {
+        name: 'StaticAssetsOAC',
+        originAccessControlOriginType: 's3',
+        signingBehavior: 'always',
+        signingProtocol: 'sigv4',
+      },
+    });
+
     // Create a CloudFront distribution
     const distribution = new cloudfront.Distribution(this, 'StaticAssetsDistribution', {
       defaultBehavior: {
-        origin: new origins.HttpOrigin(`${staticAssetsBucket.bucketName}.s3.amazonaws.com`),
+        origin: new origins.S3Origin(staticAssetsBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
       },
       defaultRootObject: 'index.html',
     });
+
+    // Add bucket policy to allow CloudFront access
+    const bucketPolicy = new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      effect: iam.Effect.ALLOW,
+      principals: [new iam.ServicePrincipal('cloudfront.amazonaws.com')],
+      resources: [staticAssetsBucket.arnForObjects('*')],
+      conditions: {
+        StringEquals: {
+          'AWS:SourceArn': distribution.distributionArn,
+        },
+      },
+    });
+
+    staticAssetsBucket.addToResourcePolicy(bucketPolicy);
 
     new cdk.CfnOutput(this, 'DistributionDomainName', {
       value: distribution.distributionDomainName,
