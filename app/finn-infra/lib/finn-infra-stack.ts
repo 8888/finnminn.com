1c44d900eab0e015a71ddf77f7d2343745272f02
8888 <leecostello1@gmail.com>
2025-06-08 15:29:04 -0400

    Add S3 bucket and CloudFront distribution for static assets in FinnInfraStack

--- app/finn-infra/lib/finn-infra-stack.ts
+++ app/finn-infra/lib/finn-infra-stack.ts
@@ -1,16 +1,34 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 
 export class FinnInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create an S3 bucket for static assets
+    const staticAssetsBucket = new s3.Bucket(this, 'StaticAssetsBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // For development - change to RETAIN for production
+      autoDeleteObjects: true, // For development - remove for production
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FinnInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create a CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'StaticAssetsDistribution', {
+      defaultBehavior: {
+        origin: new origins.HttpOrigin(`${staticAssetsBucket.bucketName}.s3.amazonaws.com`),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+        cachePolicy: cloudfront.CachePolicy.CACHING_OPTIMIZED,
+      },
+      defaultRootObject: 'index.html',
+    });
+
+    // Output the CloudFront distribution domain name
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'The domain name of the CloudFront distribution',
+    });
   }
 }
