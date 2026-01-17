3b72e5658126db31058bb561ec3bff801e0f68e7
8888 <leecostello1@gmail.com>
2026-01-16 22:53:09 -0500

    style(web/ui): update Pip icon to fangs

--- packages/ui/src/stories/AppLauncher.stories.tsx
+++ packages/ui/src/stories/AppLauncher.stories.tsx
@@ -23,8 +23,7 @@ const tiles = [
         variant="featured"
         icon={
             <svg width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M1 8c0 0 3 5 4 5s2-2 4-2 2 1 3 1 1 0 3-1 3 2 4 2 1 0 4-5 4-5-1 7-11 7S1 8 1 8z" />
-                <path d="M7 6l1 3M17 6l-1 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
+                <path d="M4 4v2h16V4H4zm2 4l1 7h2L8 8H6zm10 0l-1 7h2l1-7h-2z" />
             </svg>
         }
     />,
