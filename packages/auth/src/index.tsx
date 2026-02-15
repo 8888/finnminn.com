e0a6413fe7f8a1225b80953dd4900db8b4986a0c
8888 <leecostello1@gmail.com>
2026-02-15 12:01:21 -0500

    fix(auth): restore localStorage for persistent PWA sessions

--- packages/auth/src/index.tsx
+++ packages/auth/src/index.tsx
@@ -10,7 +10,7 @@ export const msalConfig = {
         navigateToLoginRequestUrl: false,
     },
     cache: {
-        cacheLocation: "sessionStorage",
+        cacheLocation: "localStorage",
         storeAuthStateInCookie: true,
     }
 };
